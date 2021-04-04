7b9c1370472757f3b89accb4754d811bd3b0656b
Hafidi-Alaoui Ayoub <ayoub.hafidi.alaoui@hotmail.fr>
2021-04-04 20:49:20 +0200

    generate laravel project and the relation btw entities

---
+++ Myproject/skillshub/app/Models/Exam.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Exam extends Model
+{
+    use HasFactory;
+    public function skill() {
+        return $this->belongsTo(Skill::class);
+    }
+    public function questions() {
+        return $this->hasMany(Question::class);
+    }
+    public function users() {
+        return $this->belongsTo(User::class);
+    }
+}
